64c71bf65cde1f050ff1e70409061b3de3c65edc
Awshad <mawsa3454@gmail.com>
2024-01-07 23:54:42 +0530

    WhatsappChat design sample

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text,Image,StyleSheet, View} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Chat from "./components/Chat";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return(
+      <SafeAreaView style={{gap:10}}>
+        <Text style={{textAlign:"center",fontSize:30,fontWeight:600}}>WhatsApp Chat Sample</Text>
+        <View>
+          <Chat name="Awshad" message="Hello" img="https://reactnative.dev/img/tiny_logo.png"/>  
+          <Chat name="John" message="Execuse Me" img="https://static.skillshare.com/uploads/users/233860335/user-image-large.jpg?1346349732"/>  
+          <Chat name="Tony" message="Hi there!" img="https://www.nicepng.com/png/detail/780-7805650_generic-user-image-male-man-cartoon-no-eyes.png"/>  
+          <Chat name="James" message="Good morning!" img="https://www.pngitem.com/pimgs/m/4-42408_vector-art-design-men-fashion-vector-art-illustration.png"/>  
+        </View>        
+      </SafeAreaView>    
+    
+    
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
